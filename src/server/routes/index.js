b80080017b6884b14e7e73b5fdbcc59912fd0e5b
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-07-14 17:05:25 -0600

    completed post request

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -9,22 +9,27 @@ router.post('/pokemon', function(req, res, next) {
   console.log('hitting /pokemon');
   console.log(req.body);
   Pokemon().insert({
-    name: req.body.data.name,
-    imagefront: req.body.data.sprites.front_default,
-    imageback: req.body.data.sprites.back_default,
-    type: req.body.data.types[0].type.name,
-    move1: req.body.data.moves[0].move.name,
-    move2: req.body.data.moves[1].move.name,
-    move3: req.body.data.moves[2].move.name
+    name: req.body.name,
+    imagefront: req.body.sprites.front_default,
+    imageback: req.body.sprites.back_default,
+    type: req.body.types[0].type.name,
+    move1: req.body.moves[0].move.name,
+    move2: req.body.moves[1].move.name,
+    move3: req.body.moves[2].move.name
   }, 'id').then(function(result){
     res.json({
       status: 200,
-      message: 'Succesfully saved '+req.body
+      message: 'Succesfully saved '+req.body.data.name
     });
   })
   .catch(function(err){
     console.log('errorrrrrrrrrr', err)
+    res.json({
+      status: 418,
+      message: 'I am a teapot'
+    })
   })
+  // res.json({test: 'test'})
 })
 
 module.exports = router;
